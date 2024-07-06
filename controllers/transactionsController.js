9b9fe1917313513191a174a605a7661f0497f4b5
franklinTaylor <ftaylor07@icloud.com>
2024-07-06 02:52:06 -0400

    finished my comments on each line

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,5 +1,10 @@
+// import the express framework
 const express = require("express");
+
+// initialize express router
 const router = express.Router();
+
+//import the transaction array from whatever location
 const transactionArr = require("../models/transaction");
 
 
@@ -11,12 +16,21 @@ router.get("/", (req, res) => {
 
 //get a single Transction
 router.get("/:id", (req, res) => {
+    // get the id param from the URL
     const {id} = req.params;
+
+    //find the transaction based on the "id"
     const transaction = transactionArr.find(el => el.id === +id)
+
+    //check if the transaction exist
     if (transaction) {
+        
+        // if found send a http status code 200 (ok) and the transaction obj
         res.status(200).send(transaction)
     } else {
-        res.status(400).json({error: `Transaction with id ${id} not Found`})
+
+        // if not found send a http code (400) (bad request) with and error message
+        res.status(400).json({error: `Transaction with id ${id} Does not exist try again lol ;-) `})
     }
 })
 
@@ -24,9 +38,12 @@ router.get("/:id", (req, res) => {
 
 router.post('/', (req, res) => {
     //create a new transaction objedt and increment {id} and data
+    
     const currentTransaction = {id:transactionArr.length + 1, ...req.body }
+
     //add the new transaction object to the transaction arr
     transactionArr.push(currentTransaction)
+    
     //sent a response with a http code 201 and the newly added transaction
     res.status(201).send(transactionArr[transactionArr.length-1])
 })
@@ -34,17 +51,24 @@ router.post('/', (req, res) => {
 // delete tranzaction by specified ID
 
 router.delete("/:id", (req, res) => {
+    // get the id param from the URL 
     const { id } = req.params;
     
     // Find the index of the transaction to delete
     const transactionIndex = transactionArr.findIndex(item => item.id === +id);
 
+    // Check if the transaction with the given 'id' exists
     if (transactionIndex !== -1) {
+        
         // Remove the transaction from the array
         transactionArr.splice(transactionIndex, 1);
-        res.redirect("/transactions"); // Redirect to the transactions list page
+       
+        // Redirect to the transactions list page
+        res.redirect("/transactions"); 
     } else {
-        res.status(404).json({ error: `Transaction with id: ${id} not found` });
+       
+        //if the transction isnt found send a http status code 404 (not found)
+        res.status(404).json({ error: `Transaction with id: ${id} Does not exist try again lol ;-)` });
     }
 });
 
@@ -54,15 +78,22 @@ router.delete("/:id", (req, res) => {
 router.put("/:id", (req, res) => {
     // get the id param from the request url
     const {id} = req.params;
+   
     //find the index of the transactionArr based on .id
     const transactionToUpdateIndex = transactionArr.findIndex(el => el.id === +id)
-    //check if the transaction exists
+    
+    //check if the transaction exists with "id"
     if (transactionToUpdateIndex !== -1) {
+        
         //update the transaction object in transactionArr with data from req.body
         transactionArr[transactionToUpdateIndex] = req.body
+        
+        //send a response with http code 200 (ok) and update the transction
         res.status(200).json(transactionArr[transactionToUpdateIndex])
     } else {
-        res.status(404).send({error: `Transaction with ${id} not found!`})
+        
+        //if the transction isnt found send a http status code 404 (not found)
+        res.status(404).send({error: `Transaction with ${id} Does not exist try again lol ;-)`})
     }
 
 })
